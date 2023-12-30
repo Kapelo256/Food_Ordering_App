50ecb2e96c582d7f6bb7330a7f7fc330a7009d3e
Kapelo256 <105801565+Kapelo256@users.noreply.github.com>
2023-12-30 14:38:27 -0600

    food N2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,13 +15,31 @@ export default function Home() {
           subHeader={'Our story'}
           mainHeader={'About us'}
         />
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-          Modi ex corrupti aperiam aspernatur tenetur, quaerat 
-          voluptatum quia, quis cumque veniam hic, voluptas maiores 
-          nemo neque repellat asperiores qui vero harum?
-        </p>
-
+        <div className="text-gray-500 max-w-md mx-auto mt-4 flex
+          flex-col gap-4">    
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Modi ex corrupti aperiam aspernatur tenetur, quaerat 
+            voluptatum quia, quis cumque veniam hic, voluptas maiores 
+            nemo neque repellat asperiores qui vero harum?
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Modi ex corrupti aperiam aspernatur tenetur, quaerat 
+            voluptatum quia, quis cumque veniam hic, voluptas maiores 
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Modi ex corrupti aperiam aspernatur tenetur, quaerat 
+          </p>
+        </div>
+      </section>
+      <section className="texte-center">
+      <SectionHeaders 
+          subHeader={'Don\'t hesitate'}
+          mainHeader={'Contact us'}
+        />
+        <a className="text-4xl" href="tel:+46738123123">+46 738 123 123</a>
       </section>
     </>
   )
